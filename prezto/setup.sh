6459d763795f153d6be6711a131f85dbca712cee
pinn3 <iam@lacking.education>
2016-03-07 10:34:05 +0100

    Initial prezto setup script

---
+++ prezto/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+cd "${ZDOTDIR:-$HOME}" &&
+git clone https://github.com/pinn3/prezto "${ZDOTDIR:-$HOME}/.zprezto" &&
+cd "${ZDOTDIR:-$HOME}"/.zprezto &&
+
+setopt EXTENDED_GLOB &&
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
