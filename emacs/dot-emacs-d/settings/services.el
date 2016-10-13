521ebc1b08c8c7a86ca294a6b8692a45e6fa1ea4
pinn3 <iam@lacking.education>
2016-10-13 13:59:59 +0200

    Add prodigy services for Tiny Collect

--- emacs/dot-emacs-d/settings/services.el
+++ emacs/dot-emacs-d/settings/services.el
@@ -33,6 +33,35 @@
   :kill-signal 'sigkill
   :kill-process-buffer-on-stop t)
 
+;; Tiny Collect app prodigy services
+;;; code:
+(prodigy-define-service
+  :name "tCollect API"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/tiny-collect"
+  :tags '(personal tcollect)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "tCollect server"
+  :command "npm"
+  :args '("run" "serve")
+  :cwd "~/git/tiny-collect"
+  :tags '(personal tcollect)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "tCollect watcher"
+  :command "npm"
+  :args '("run" "watch")
+  :cwd "~/git/tiny-collect"
+  :tags '(personal tcollect)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
 
 (provide 'services)
 ;;; services.el ends here
