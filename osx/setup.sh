625fd551ffb00f63a3995873f794dd628a974916
pinn3 <iam@lacking.education>
2016-11-18 21:23:30 +0100

    Rename hammerspoon source directory

--- osx/setup.sh
+++ osx/setup.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict &&
-ln -s $DIR/dot-hammerspoon ~/.hammerspoon &&
+ln -s $DIR/hammerspoon ~/.hammerspoon &&
 echo "Symlinked osx dotfiles." &&
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
