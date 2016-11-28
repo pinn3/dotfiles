2e0dfba409f99530e0d22171b174be2bab678561
pinn3 <iam@lacking.education>
2016-11-28 13:35:38 +0100

    Add happi services

--- emacs/dot-emacs-d/settings/ispy.el
+++ emacs/dot-emacs-d/settings/ispy.el
@@ -32,6 +32,24 @@
   :kill-signal 'sigkill
   :kill-process-buffer-on-stop t)
 
+(prodigy-define-service
+  :name "Happi API"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/happi/server"
+  :tags '(work ispy hub)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Happi Frontend"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/happi/www"
+  :tags '(work ispy hub)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
 
 (provide 'ispy)
 ;;; ispy.el ends here
