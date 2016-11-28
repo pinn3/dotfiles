75351150bd87549c3eca626a37869a0ecfc885b4
pinn3 <iam@lacking.education>
2016-11-28 13:38:40 +0100

    Add trashy bash configuration

---
+++ bash/modules/trash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Bash trash, configuration that i don't really want
+#
+
+export NVM_DIR=/Users/pinn3/.nvm
+. /usr/local/opt/nvm/nvm.sh
+
+alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
