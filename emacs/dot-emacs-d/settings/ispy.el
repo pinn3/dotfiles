f8776fbe8e628709125b3a314e6f51f43e70126e
pinn3 <iam@lacking.education>
2017-02-10 00:53:58 +0100

    Add melobee and moank prodigy services

--- emacs/dot-emacs-d/settings/ispy.el
+++ emacs/dot-emacs-d/settings/ispy.el
@@ -37,7 +37,7 @@
   :command "npm"
   :args '("run" "dev")
   :cwd "~/git/ispy/happi/server"
-  :tags '(work ispy hub)
+  :tags '(work ispy hub backend)
   :kill-signal 'sigkill
   :kill-process-buffer-on-stop t)
 
@@ -47,6 +47,51 @@
   :args '("run" "dev")
   :cwd "~/git/ispy/happi/www"
   :tags '(work ispy hub)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Melobee API"
+  :command "npm"
+  :args '("run" "dev-local")
+  :cwd "~/git/ispy/melobee/api"
+  :tags '(work ispy mel backend)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Melobee frontend"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/melobee/www"
+  :tags '(work ispy mel)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Moank API"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/moank/api"
+  :tags '(work ispy moan)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Moank frontend"
+  :command "npm"
+  :args '("run" "dev-server")
+  :cwd "~/git/ispy/moank/frontend"
+  :tags '(work ispy moan)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Moank backend"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/moank/backend"
+  :tags '(work ispy moan)
   :kill-signal 'sigkill
   :kill-process-buffer-on-stop t)
 
