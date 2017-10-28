d9429653a906f9baae8113fd2031e6baffe2def9
pinn3 <iam@lacking.education>
2017-10-28 16:32:52 +0200

    Add more ISPY services to prodigy config

--- emacs/dot-emacs-d/settings/ispy.el
+++ emacs/dot-emacs-d/settings/ispy.el
@@ -24,6 +24,24 @@
   :kill-process-buffer-on-stop t)
 
 (prodigy-define-service
+  :name "GRCW API"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/grcw/api"
+  :tags '(work ispy grcw backend)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "GRCW frontend"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/grcw/www"
+  :tags '(work ispy grcw)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
   :name "Happi orderflow frontend"
   :command "npm"
   :args '("run" "dev")
@@ -69,6 +87,33 @@
   :kill-process-buffer-on-stop t)
 
 (prodigy-define-service
+  :name "Melobee UI"
+  :command "yarn"
+  :args '("dev")
+  :cwd "~/git/ispy/melobee/ui"
+  :tags '(work ispy mel)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Melobee artist landing"
+  :command "yarn"
+  :args '("dev")
+  :cwd "~/git/ispy/melobee/artist-landing"
+  :tags '(work ispy mel)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "Melobee CMS"
+  :command "yarn"
+  :args '("dev")
+  :cwd "~/git/ispy/melobee/cms"
+  :tags '(work ispy mel)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
   :name "Moank API"
   :command "npm"
   :args '("run" "dev")
@@ -78,9 +123,18 @@
   :kill-process-buffer-on-stop t)
 
 (prodigy-define-service
+  :name "Moank backend"
+  :command "npm"
+  :args '("run" "dev")
+  :cwd "~/git/ispy/moank/backend"
+  :tags '(work ispy moan)
+  :kill-signal 'sigkill
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
   :name "Moank frontend"
   :command "npm"
-  :args '("run" "dev-server")
+  :args '("run" "dev")
   :cwd "~/git/ispy/moank/frontend"
   :tags '(work ispy moan)
   :kill-signal 'sigkill
