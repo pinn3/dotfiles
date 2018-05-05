2f17906d9d44908cc60ba792d2615075863e1a86
pinn3 <iam@lacking.education>
2018-05-06 00:09:45 +0200

    Fix git config setup script

--- git/setup.sh
+++ git/setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo "work/personal?: "
-read type
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/gitignore_global ~/.gitignore_global &&
-ln -s $DIR/gitconfig_$type ~/.gitconfig &&
+ln -s $DIR/gitconfig ~/.gitconfig &&
 echo "Symlinked git dotfiles."
 
