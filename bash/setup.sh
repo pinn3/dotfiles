ba5f127027a6d2b391c1829a575088410e910952
pinn3 <iam@lacking.education>
2018-12-16 02:08:28 +0100

    Introduce custom bash binaries folder

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,5 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/bashrc ~/.bashrc &&
+ln -s $DIR/binaries ~/.bash_binaries &&
 ln -s $DIR/modules ~/.bash_modules &&
 echo "Symlinked bash dotfiles."
