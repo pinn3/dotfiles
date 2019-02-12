3754bb827468fa757ba9fbc554b326d6e483cd1a
pinn3 <iam@lacking.education>
2019-02-12 22:09:57 +0100

    Migrate configuration from legacy gui.el to theme.el and keybindings.el, removing it completely

--- emacs/dot-emacs-d/configuration/theme.el
+++ emacs/dot-emacs-d/configuration/theme.el
@@ -16,5 +16,57 @@
   :background (face-attribute 'default :background))
 
 
+;; Disable Menu Bar, Scroll Bar, tool bar
+(menu-bar-mode -1)
+;(scroll-bar-mode -1)
+;(tool-bar-mode -1)
+
+
+;; Highlight matching paranthesis and no limit to distance of matching parenthesis
+(show-paren-mode 1)
+(setq blink-matching-paren-distance nil)
+(defadvice show-paren-function
+  (after show-matching-paren-offscreen activate)
+  "If the matching paren is offscreen, show the matching line in the echo area.
+Has no effect if the character before point is not of the syntax class ')'."
+  (interactive)
+  (let* ((cb (char-before (point)))
+	 (matching-text (and cb
+			     (char-equal (char-syntax cb) ?\) )
+			     (blink-matching-open))))
+    (when matching-text (message matching-text))))
+
+
+;; Indent-guides
+(require 'indent-guide)
+(indent-guide-global-mode)
+;(setq indent-guide-recursive t)
+(setq indent-guide-delay 0.2)
+
+
+;; Helm
+(require 'helm)
+(helm-autoresize-mode 1)
+(setq helm-autoresize-max-height 11)
+(setq helm-autoresize-min-height 11)
+(setq helm-display-header-line nil)
+(set-face-attribute 'helm-source-header nil :height 0.1)
+(setq helm-split-window-in-side-p t)
+(set-face-attribute 'helm-selection nil
+                    :background "#333"
+                    :foreground "#fff")
+(set-face-attribute 'helm-match nil
+                    :background "#343030")
+
+(set-face-attribute 'helm-source-header nil
+                    :background "#a9df90"
+                    :foreground "#706565")
+
+(advice-add 'helm-ff-filter-candidate-one-by-one
+            :around (lambda (fcn file)
+                      (unless (string-match "\\(?:/\\|\\`\\)\\.\\{1,2\\}\\'" file)
+                                            (funcall fcn file))))
+
+
 (provide 'theme)
 ;;; theme.el ends here
--- emacs/dot-emacs-d/init.el
+++ emacs/dot-emacs-d/init.el
@@ -27,7 +27,6 @@
 (load "behaviour")
 (load "functions")
 (load "modes")
-(load "gui")
 
 
 (require 'yasnippet)
