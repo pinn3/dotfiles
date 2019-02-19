cd523529863be3d83ce1fd7626276ecb5ba7edf3
pinn3 <iam@lacking.education>
2019-02-19 12:41:56 +0100

    Add blacken (python formatter) to emacs

--- emacs/dot-emacs-d/flowbox.el
+++ emacs/dot-emacs-d/flowbox.el
@@ -2,7 +2,11 @@
 ;;; Commentary:
 
 ;;; Code:
-(setq-default flycheck-python-pycompile-executable "~/git/flowbox/aws-lambda/python/lambda-py/bin/python3")
+(setq-default flycheck-python-pycompile-executable "~/git/flowbox-deployment-envs/3.6/bin/python")
+
+;; Hacky setup of blacken
+(require 'blacken)
+(setq blacken-executable "~/git/flowbox-deployment-envs/3.6/bin/black")
 
 
 (provide 'flowbox)
