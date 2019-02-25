afd5cfd7ed25814903a072bb4f2d4155cf3c74fc
pinn3 <iam@lacking.education>
2019-02-25 15:46:35 +0100

    Remove redundant emacs macOS keybind fixes

--- emacs/dot-emacs-d/keybindings.el
+++ emacs/dot-emacs-d/keybindings.el
@@ -43,52 +43,6 @@
 (global-set-key [f4] 'kmacro-end-or-call-macro)
 
 
-;; Mac meta-key overrides
-(defun mac-osx-editing-insert-at ()
-  "Insert @ at point."
-  (interactive)
-  (insert-char ?@ 1))
-(defun mac-osx-editing-insert-curly-left ()
-  "Insert { at point."
-  (interactive)
-  (insert-char ?{ 1))
-(defun mac-osx-editing-insert-curly-right ()
-  "Insert } at point."
-  (interactive)
-  (insert-char ?} 1))
-(defun mac-osx-editing-insert-bracket-left ()
-  "Insert [ at point."
-  (interactive)
-  (insert-char ?[ 1))
-(defun mac-osx-editing-insert-bracket-right ()
-  "Insert ] at point."
-  (interactive)
-  (insert-char ?] 1))
-(defun mac-osx-editing-insert-dollar ()
-  "Insert $ at point."
-  (interactive)
-  (insert-char ?$ 1))
-(defun mac-osx-editing-insert-pipe ()
-  "Insert | at point."
-  (interactive)
-  (insert-char ?| 1))
-(defun mac-osx-editing-insert-back-slash ()
-  "Insert \ at point."
-  (interactive)
-  (insert-char ?\\ 1))
-(global-set-key (kbd "M-2") 'mac-osx-editing-insert-at)
-(global-set-key (kbd "M-4") 'mac-osx-editing-insert-dollar)
-(global-set-key (kbd "M-7") 'mac-osx-editing-insert-pipe)
-(global-set-key (kbd "M-/") 'mac-osx-editing-insert-back-slash)
-(global-set-key (kbd "M-8") 'mac-osx-editing-insert-bracket-left)
-(global-set-key (kbd "M-9") 'mac-osx-editing-insert-bracket-right)
-(global-set-key (kbd "M-(") 'mac-osx-editing-insert-curly-left)
-(global-set-key (kbd "M-)") 'mac-osx-editing-insert-curly-right)
-
-;; Mac shift-key fix
-(define-key input-decode-map "\e[1;2A" [S-up])
-
-
 ;; Helm
 (require 'helm)
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
