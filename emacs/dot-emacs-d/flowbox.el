b0c3152d37282375b51edbab9d60f66e63b2ee1d
pinn3 <iam@lacking.education>
2019-02-28 02:21:34 +0100

    Simplify blacken-region by using with-temp-buffer

--- emacs/dot-emacs-d/flowbox.el
+++ emacs/dot-emacs-d/flowbox.el
@@ -3,28 +3,25 @@
 
 ;;; Code:
 (setq-default flycheck-python-pycompile-executable "~/git/flowbox-deployment-envs/3.6/bin/python")
+(setq-default flycheck-python-flake8-executable "~/git/flowbox-deployment-envs/3.6/bin/flake8")
 
 ;; Hacky setup of blacken
 (require 'blacken)
 (setq blacken-executable "~/git/flowbox-deployment-envs/3.6/bin/black")
+
 (defun blacken-region (beg end)
   "Try to blacken the current region (between BEG and END)."
   (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
                  (list nil nil)))
-  (let* ((origbuf (current-buffer))
-         (origbeg beg)
-         (origend end)
-         (regionbuf (get-buffer-create "*blacken-region*"))
-         (content (buffer-substring-no-properties beg end)))
-    (with-current-buffer regionbuf
-      (erase-buffer)
-      (insert content)
-      (blacken-buffer)
-      (with-current-buffer origbuf
-        (delete-region origbeg origend)
-        (insert-buffer-substring regionbuf)))
-    (kill-buffer regionbuf)))
+  (let* ((content (buffer-substring-no-properties beg end))
+         (blackened (with-temp-buffer
+                      (insert content)
+                      (blacken-buffer)
+                      (buffer-string))))
+    (with-current-buffer (current-buffer)
+      (delete-region beg end)
+      (insert blackened))))
 
 
 (provide 'flowbox)
