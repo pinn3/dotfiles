8f494d2ff7ebcf6f7ceb00e39f3ae51ec828122c
pinn3 <iam@lacking.education>
2020-11-08 15:55:28 +0100

    Add aws bash alias

--- bash/modules/trash.sh
+++ bash/modules/trash.sh
@@ -5,3 +5,4 @@
 
 alias trees='tree -L 1'
 alias celar='echo "Down we gOOoooOOoooOoo"'
+alias aws="docker run --rm -it -v $HOME/.aws/config:/root/.aws/config:ro -v $HOME/.aws/credentials:/root/.aws/credentials:ro amazon/aws-cli:2.0.7"
