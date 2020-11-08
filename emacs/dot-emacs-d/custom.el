ec249f43130fbe61e51e817a6dd99a101370f9ce
pinn3 <iam@lacking.education>
2020-11-08 15:45:58 +0100

    Use quote symbol in custom.el
    Seems like this was changed in emacs 27

--- emacs/dot-emacs-d/custom.el
+++ emacs/dot-emacs-d/custom.el
@@ -4,11 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" default)))
+   '("3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" default))
  '(package-selected-packages
-   (quote
-    (jinja2-mode blacken helm-pass dockerfile-mode base16-theme ace-window terraform-mode expand-region yasnippet yaml-mode web-mode scad-mode neotree markdown-mode lua-mode julia-mode indent-guide helm go-mode flycheck emmet-mode editorconfig csv-mode auto-complete ace-jump-mode abc-mode))))
+   '(jinja2-mode helm-pass blacken dockerfile-mode base16-theme ace-window terraform-mode expand-region yasnippet yaml-mode web-mode scad-mode neotree markdown-mode lua-mode julia-mode indent-guide helm go-mode flycheck emmet-mode editorconfig csv-mode auto-complete ace-jump-mode abc-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
