09c4dcec4b5be6b97615e4021bf6f6bc58eebe08
pinn3 <iam@lacking.education>
2021-03-17 06:57:51 +0100

    Add basic alacritty configuration

--- generic/setup.sh
+++ generic/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -s $DIR/alacritty.yml ~/.alacritty.yml &&
 ln -s $DIR/screenrc ~/.screenrc &&
 ln -s $DIR/sqliterc ~/.sqliterc &&
 echo "Symlinked generic dotfiles."
