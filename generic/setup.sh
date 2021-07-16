13d0ce0b345bb31226611c080580d1972183eea4
pinn3 <iam@lacking.education>
2021-07-16 20:11:22 +0200

    Add keynav configuration

--- generic/setup.sh
+++ generic/setup.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/alacritty.yml ~/.alacritty.yml &&
+(mkdir -p ~/.config/keynav/ && ln -s $DIR/keynavrc ~/.config/keynav/keynavrc) &&
 ln -s $DIR/screenrc ~/.screenrc &&
 ln -s $DIR/sqliterc ~/.sqliterc &&
 echo "Symlinked generic dotfiles."
-
