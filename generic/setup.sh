d12c65f1b4c10fc54712e4da45b21973873cdd96
pinn3 <iam@lacking.education>
2021-07-16 20:12:24 +0200

    Prevent setup script for generic dotfiles form crashing
    Instead of exiting as soon as it fails to symlink one of the files, it
    will continue trying to link the rest of them before existing

--- generic/setup.sh
+++ generic/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -s $DIR/alacritty.yml ~/.alacritty.yml &&
-(mkdir -p ~/.config/keynav/ && ln -s $DIR/keynavrc ~/.config/keynav/keynavrc) &&
-ln -s $DIR/screenrc ~/.screenrc &&
-ln -s $DIR/sqliterc ~/.sqliterc &&
-echo "Symlinked generic dotfiles."
+
+(mkdir -p ~/.config/alacritty/ && ln -s $DIR/alacritty.yml ~/.config/alacritty/alacritty.yml && echo "Symlinked Alacritty configuration")
+(mkdir -p ~/.config/keynav/ && ln -s $DIR/keynavrc ~/.config/keynav/keynavrc && echo "Symlinked keynav configuration")
+(ln -s $DIR/screenrc ~/.screenrc && echo "Symlinked GNU screen configuration")
+(ln -s $DIR/sqliterc ~/.sqliterc && echo "Symlinked SQLite configuration")
