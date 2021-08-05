c2e67f04218ff7254393769b3c6d379f1b57873b
pinn3 <iam@lacking.education>
2021-08-06 00:54:41 +0200

    Mark startup org-mode source block as safe
    Preventing emacs from prompting for permission to execute it every time
    I open my notes.org document

--- emacs/dot-emacs-d/custom.el
+++ emacs/dot-emacs-d/custom.el
@@ -7,6 +7,10 @@
    '("3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" default))
  '(package-selected-packages
    '(company lsp-mode jinja2-mode helm-pass blacken dockerfile-mode base16-theme ace-window terraform-mode expand-region yasnippet yaml-mode web-mode scad-mode neotree markdown-mode lua-mode julia-mode indent-guide helm go-mode flycheck emmet-mode editorconfig csv-mode ace-jump-mode abc-mode)))
+ '(safe-local-variable-values
+   '((eval progn
+           (org-babel-goto-named-src-block "startup")
+           (org-babel-execute-src-block)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
