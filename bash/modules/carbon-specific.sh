dd2f0e1141b35f8d195c248266f5c48e2525cadd
pinn3 <iam@lacking.education>
2021-10-15 21:08:36 +0200

    Set up nix and direnv in carbon configuration

--- bash/modules/carbon-specific.sh
+++ bash/modules/carbon-specific.sh
@@ -1,3 +1,6 @@
+if [ -e /home/pinn3/.nix-profile/etc/profile.d/nix.sh ]; then . /home/pinn3/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+eval "$(direnv hook bash)"
+
 cat /etc/motd
 
 export PATH=$PATH:${HOME}/scripts
