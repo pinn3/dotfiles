6ffb6eb1eaba849348f28cd188d3617497dbe372
pinn3 <iam@lacking.education>
2021-10-16 04:02:17 +0200

    Remove bash binaries directory
    Only used them once or twice each

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,6 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/bashrc ~/.bashrc &&
-ln -s $DIR/binaries ~/.bash_binaries &&
 ln -s $DIR/modules ~/.bash_modules &&
 echo "Symlinked bash dotfiles."
