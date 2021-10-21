e4c877e1b42aa83e77d5c6516989c48221f0858d
pinn3 <iam@lacking.education>
2021-10-21 06:37:57 +0200

    Remove unused .screenrc and .sqliterc

--- generic/setup.sh
+++ generic/setup.sh
@@ -4,5 +4,3 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 (mkdir -p ~/.config/alacritty/ && ln -s $DIR/alacritty.yml ~/.config/alacritty/alacritty.yml && echo "Symlinked Alacritty configuration")
 (mkdir -p ~/.config/keynav/ && ln -s $DIR/keynavrc ~/.config/keynav/keynavrc && echo "Symlinked keynav configuration")
-(ln -s $DIR/screenrc ~/.screenrc && echo "Symlinked GNU screen configuration")
-(ln -s $DIR/sqliterc ~/.sqliterc && echo "Symlinked SQLite configuration")
