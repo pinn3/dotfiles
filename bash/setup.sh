afb963b1ca1ceaadbb5a3925aa15e7f9e00d29fe
pinn3 <iam@lacking.education>
2021-10-21 16:02:36 +0200

    Pin z.sh module to latest revision

--- bash/setup.sh
+++ bash/setup.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Downloading base16-tomorrow-night.sh module" && \
 curl "https://raw.githubusercontent.com/chriskempson/base16-shell/023727241e448ee37b478261c3dda1121395cbc8/scripts/base16-tomorrow-night.sh" -o $DIR/modules/base16-tomorrow-night.sh && \
 echo "Downloading z.sh module" && \
-curl "https://raw.githubusercontent.com/rupa/z/78f0d97d3dd334109fb42502c1b3d46c9ab3482d/z.sh" -o $DIR/modules/z.sh && \
+curl "https://raw.githubusercontent.com/rupa/z/b82ac78a2d4457d2ca09973332638f123f065fd1/z.sh" -o $DIR/modules/z.sh && \
 ln -s $DIR/bashrc ~/.bashrc && \
 echo "Symlinked bash dotfiles."
