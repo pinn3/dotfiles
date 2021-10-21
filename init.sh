f81c18fb66e11b40ef31eea6b43dd5d1594c548b
pinn3 <iam@lacking.education>
2021-10-21 06:35:18 +0200

    Remove OSX/MacOS specific configuration

--- init.sh
+++ init.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
 ./bash/setup.sh
-
-read -r -p "Are you on OSX? [y/N] " response
-case $response in
-  [yY][eE][sS]|[yY])
-    ./osx/setup.sh
-    ;;
-esac
-
 ./generic/setup.sh
 ./git/setup.sh
 ./emacs/setup.sh
