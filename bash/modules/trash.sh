667f4fa65d20bab106f29c2239cc7c55098214d7
pinn3 <iam@lacking.education>
2021-10-21 06:29:09 +0200

    Remove trashy bash configuration

--- bash/modules/trash.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# Bash trash, configuration that i don't really want
-#
-
-alias trees='tree -L 1'
-alias celar='echo "Down we gOOoooOOoooOoo"'
-alias aws="docker run --rm -it -v $HOME/.aws/config:/root/.aws/config:ro -v $HOME/.aws/credentials:/root/.aws/credentials:ro amazon/aws-cli:2.0.7"
