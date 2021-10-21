6a8175bd32a244d27a7b22a8488018a13b8eefb3
pinn3 <iam@lacking.education>
2021-10-21 15:56:44 +0200

    Automatically download bash modules

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -s $DIR/bashrc ~/.bashrc &&
+
+echo "Downloading base16-tomorrow-night.sh module" && \
+curl "https://raw.githubusercontent.com/chriskempson/base16-shell/023727241e448ee37b478261c3dda1121395cbc8/scripts/base16-tomorrow-night.sh" -o $DIR/modules/base16-tomorrow-night.sh && \
+echo "Downloading z.sh module" && \
+curl "https://raw.githubusercontent.com/rupa/z/78f0d97d3dd334109fb42502c1b3d46c9ab3482d/z.sh" -o $DIR/modules/z.sh && \
+ln -s $DIR/bashrc ~/.bashrc && \
 echo "Symlinked bash dotfiles."
